049bc2607781eb5cbf2bcaa403abf87d9c43d656
Rodrigo Campos <32779262+rodrigocamposbr@users.noreply.github.com>
2023-10-17 11:21:06 +0100

    Update entrypoint.sh

--- .github/actions/php/entrypoint.sh
+++ .github/actions/php/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 echo $INPUT_LANGUAGE_VERSION
+
+cd /docker-action
+
+docker build -t docker-action --build-arg INPUT_LANGUAGE_VERSION="$INPUT_LANGUAGE_VERSION" . && docker run docker-action
